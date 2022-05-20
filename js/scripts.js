c72674d44550fe392044d41aab0a119dda9ffe25
bahlaZakariyaa <zakariyaa.bahla@gmail.com>
2022-05-20 16:08:16 +0100

    problem of responsive navbar is fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,60 @@
-const oMenu = document.querySelector('#open-menu');
-const cMenu = document.querySelector('#close-menu');
-const links = document.querySelector('#links');
+(function () {
+    'use strict';
 
-const ulist = document.querySelector('#links ul');
-const navRow = document.querySelector('nav .row');
+    const openMenu = document.querySelector('#open-menu');
+    const closeMenu = document.querySelector('#close-menu');
+    const links = document.querySelectorAll('.nav-link');
+    const menu = document.querySelector('#links');
 
-oMenu.addEventListener('click', event=>{
-    event.preventDefault();
-        
-    links.style.display = 'inline-block';
-    cMenu.style.display = 'block';
-    ulist.className = 'vertical-display';
+    openMenu.addEventListener('click', event => {
+        event.preventDefault();
+        openMenu.style.display = 'none';
+        menu.style.display = 'inline-block';
+        closeMenu.style.display = 'inline-block';
 
-        cMenu.addEventListener('click', event=>{
-            event.preventDefault();
-            
-            links.style.display= 'none';
-            oMenu.style.display = 'inline-block';
+        links.forEach(link => {
+            link.addEventListener('click', () => {
+                menu.style.display = 'none';
+                openMenu.style.display = 'inline-block';
+            });
         });
-});
 
-
-
-// Scrolling function
-const height = document.getElementById('header').clientHeight;
-$(window).scroll(()=>{
-    if ($(this).scrollTop() > height) {
-        $('nav').addClass('fixed');
-    }else{
-        $('nav').removeClass('fixed');        
-    }
-});
-
-const scrollUpBtns = document.querySelectorAll('.contactMe .scroll-up');
-scrollUpBtns.forEach(scrollUpBtn=>{
-    scrollUpBtn.addEventListener('click',event=>{
-        event.preventDefault();
-        window.scrollTo(0, height);
+        closeMenu.addEventListener('click', event => {
+            event.preventDefault();
+            menu.style.display = 'none';
+            openMenu.style.display = 'inline-block';
+        });
     });
-});
 
-
-
-const aboutSec = document.querySelector('#about-us');
-const aboutDistanceToTop = window.pageYOffset + aboutSec.getBoundingClientRect().top + '200' ;
-console.log(aboutDistanceToTop);
-const scrollToAboutBtns = document.querySelectorAll('.hero .scroll-down');
-scrollToAboutBtns.forEach(scrollToAboutBtn=>{
-    scrollToAboutBtn.addEventListener('click',event=>{
-        event.preventDefault();
-        window.scrollTo(0, aboutDistanceToTop);
+    // Scrolling function
+    const height = document.getElementById('header').clientHeight;
+    $(window).scroll(() => {
+        if ($(this).scrollTop() > height) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
     });
-});
+
+    const scrollUpBtns = document.querySelectorAll('.contactMe .scroll-up');
+    scrollUpBtns.forEach(scrollUpBtn => {
+        scrollUpBtn.addEventListener('click', event => {
+            event.preventDefault();
+            window.scrollTo(0, height);
+        });
+    });
+
+
+
+    const aboutSec = document.querySelector('#about-us');
+    const aboutDistanceToTop = window.pageYOffset + aboutSec.getBoundingClientRect().top + '200';
+    console.log(aboutDistanceToTop);
+    const scrollToAboutBtns = document.querySelectorAll('.hero .scroll-down');
+    scrollToAboutBtns.forEach(scrollToAboutBtn => {
+        scrollToAboutBtn.addEventListener('click', event => {
+            event.preventDefault();
+            window.scrollTo(0, aboutDistanceToTop);
+        });
+    });
+
+})();
